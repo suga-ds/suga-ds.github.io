96ba2a308cfe27138924b98511f12c2644a613b2
suga-ds <pmpnicolas@gmail.com>
2024-07-29 21:27:29 +0200

    revision

--- cartography/mainRefugisClimàtics.js
+++ cartography/mainRefugisClimàtics.js
@@ -6,7 +6,7 @@ var customIcon = L.icon({
 });
 
 var enlargedIcon = L.icon({
-  iconUrl: './dot.png', // Path to your image
+  iconUrl: './images/dot.png', // Path to your image
   iconSize: [20, 20], // Enlarged size of the icon
 });
 
@@ -32,71 +32,20 @@ fetch('./cartography/refugisClimàtics.json')
   })
   .catch(error => console.error('Error fetching the JSON data:', error));
 
-// Add a control with the "localització" text
 var localitzacioControl = L.control({ position: 'topright' });
 
 localitzacioControl.onAdd = function (map) {
   var div = L.DomUtil.create('div', 'localitzacio-label');
-  div.innerHTML = 'LOCALITZACIÓ REFUGIS CLIMÀTICS';
-  div.style.backgroundColor = '#f8f8f8';
-  this._div.style.padding = '1vh'; // Padding for better spacing
-  this._div.style.borderRadius = '0.4vh'; // Rounded corners
-  this._div.style.fontSize = '1.6vh'; // Font size
-return div;
+  div.innerHTML = '<h4>REFUGIS CLIMÀTICS A BARCELONA</h4>';
+  div.style.backgroundColor = 'rgb(248, 248, 248, 0.8)'; // Background color with transparency
+  div.style.padding = '1vh'; // Padding for better spacing
+  div.style.borderRadius = '0.4vh'; // Rounded corners
+  div.style.fontSize = '1.6vh'; // Font size
+
+
+
+
+  return div;
 };
 
 localitzacioControl.addTo(map3);
-
-
-/*
-// Fetch and display the districtes data
-fetch('./cartography/barcelonaDistrictes.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        color: "#000",
-        // fillColor: "#3867A8",
-        weight: 1,
-        opacity: 0.2,
-        fillOpacity: 0
-      },
-      onEachFeature: function (feature, layer) {
-        if (feature.properties && feature.properties.name) {
-          layer.bindPopup(feature.properties.name);
-        }
-      }
-    }).addTo(map3);
-  })
-  .catch(error => console.error('Error fetching districts data:', error));
-
-fetch('./cartography/barcelonaBarris.json')
-  .then(response => response.json())
-  .then(data => {
-    // Loop through the records in the JSON file
-    data.result.records.forEach(function (record) {
-      // Parse the WGS84 geometry
-      var wgs84Coordinates = record.geometria_wgs84
-        .replace('POLYGON ((', '')
-        .replace('))', '')
-        .split(', ')
-        .map(coord => coord.split(' ').map(Number));
-
-      // Create an array of LatLng objects
-      var latLngs = wgs84Coordinates.map(coord => [coord[1], coord[0]]);
-
-      // Define style options
-      var polygonStyle = {
-        color: "#000",
-        // fillColor: "#3867A8",
-        weight: 0.5,
-        opacity: 0.2,
-        fillOpacity: 0
-      };
-
-      // Add the polygon to the map with style options
-      L.polygon(latLngs, polygonStyle).addTo(map3);
-    });
-  })
-  .catch(error => console.error('Error fetching the JSON data:', error));
-*/
